29d5de93ed51eefd46733de7a7f090e2c375486e
dawit <35230675+dawit0905@users.noreply.github.com>
2022-01-04 00:17:45 +0900

    Update baekjoon_1009.cpp

--- cpp/baekjoon_1009.cpp
+++ cpp/baekjoon_1009.cpp
@@ -6,8 +6,8 @@ int main() {
 	while(t--) {
 		mul = 1;
 		cin >> a >> b;
-		b = !(b%4)? 4 : b%4;
-		while (b--)mul *= a;
+		b = (b%4) ? b%4 : 4;
+		while (b--) mul *= a;
 		cout << (mul%10?mul%10:10) << "\n";
 	}
 }
