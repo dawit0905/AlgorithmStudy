a4861c06a3109b2a8635fb6ab857b61d77bba76c
dawit <dawit0905@gmail.com>
2022-01-27 22:41:33 +0900

    일반적인 백트래킹 알고리즘

---
+++ 개념/백트래킹/일반적인 백트래킹 알고리즘.py
@@ -0,0 +1,10 @@
+def checknode(node):
+    if(promising(node)):
+        if(node에 해답이있으면):
+            해당을출력
+        else :
+            for (node의 모든 자식노드 u에 대해서):
+                checknode(node)
+
+def promising(node):
+    pass
