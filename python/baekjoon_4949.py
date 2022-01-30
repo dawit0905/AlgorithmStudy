ec1e0a54fcc34b97ca4cbb022c9142fa85f1eca8
dawit <dawit0905@gmail.com>
2022-01-31 00:11:11 +0900

    update baekjoon_4949.py

--- python/baekjoon_4949.py
+++ python/baekjoon_4949.py
@@ -2,17 +2,19 @@ import sys
 
 
 while 1:
+    # 데이터 입력
     input_data = sys.stdin.readline().rstrip()
-    array = []
     # 종료 조건
     if input_data == '.':
         break
     # 괄호 합계
     parenthesis_sum = 0
     square_brackets_sum = 0
-
-    # 특이 케이스 처리
+    # 괄호를 담을 배열
+    array = []
+    # 현재값이 ) 이거나 ] 일때 array에서 꺼낸 값(바로 앞에 있는 괄호가 [ or ( 이면 no을 하기 위한 변수
     temp = ''
+
     for i in input_data:
         if i == '(' :
             parenthesis_sum += 1
@@ -31,7 +33,6 @@ while 1:
                 break
             temp = array.pop()
 
-
         if len(array) > 0:
             if temp == '(' and i == ']':
                 break
@@ -40,5 +41,5 @@ while 1:
 
     if parenthesis_sum == 0 and square_brackets_sum == 0:
         print('yes')
-    else :
+    else:
         print('no')
