ffbc806ab1864166b94589a21d87e202dc8f1f37
dawit <dawit0905@gmail.com>
2022-02-05 01:32:09 +0900

    add baekjoon_1927.py

---
+++ python/baekjoon_1927.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+max_heap = []
+for i in range(n):
+    input_num = int(sys.stdin.readline())
+    if input_num == 0:
+        if max_heap:
+            print(heapq.heappop(max_heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(max_heap, input_num)
+
