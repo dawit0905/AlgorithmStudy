163e10720c494d595561a206d52486683933243e
dawit <dawit0905@gmail.com>
2022-02-15 20:12:39 +0900

    update baekjoon_2178.py

--- python/baekjoon_2178.py
+++ python/baekjoon_2178.py
@@ -1,32 +1,36 @@
+import sys
 from collections import deque
 
-def bfs(x,y):
-    queue = deque()
-    queue.append((x,y))
 
-    while queue:
-        x,y = queue.popleft()
+def bfs(x, y):
+    que = deque()
+    que.append((x, y))
+
+    while que:
+        x, y = que.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx <0 or nx >=n or ny < 0 or ny >= m:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if graph[nx][ny] == 0:
+            if array[nx][ny] == 0:
                 continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx,ny))
+            if array[nx][ny] == 1:
+                array[nx][ny] = array[x][y] + 1
+                que.append((nx, ny))
+    return array[n-1][m-1]
 
-    return graph[n-1][m-1]
-
-n,m = map(int, input().split())
-graph = []
+n, m = map(int, sys.stdin.readline().split())
+array = []
 for i in range(n):
-    graph.append(list(map(int, input())))
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-print(bfs(0,0))
+    array.append(list(map(int, sys.stdin.readline().rstrip())))
 
-
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+print(bfs(0, 0))
+# for i in array:
+#     for j in range(m):
+#         print(i[j], end=" ")
+#     print()
