85328a85f77c842a946d72209bf0879e2adb3c5d
dawit <dawit0905@gmail.com>
2022-03-10 20:50:31 +0900

    add baekjoon_1158.py

---
+++ python/baekjoon_1158.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+
+answer = []
+arr = [i for i in range(1, n+1)]
+index = 0
+for i in range(n):
+    index = index + (k-1)
+    if index != 0:
+        index = index % len(arr)
+    answer.append(str(arr.pop(index)))
+
+print("<", ", ".join(answer)[:], ">", sep='')
