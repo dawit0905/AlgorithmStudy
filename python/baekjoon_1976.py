b68bee712cbeb35f24974ac28dc4a0e9a06400d5
dawit <dawit0905@gmail.com>
2022-03-21 22:20:02 +0900

    update baekjoon_1976.py

--- python/baekjoon_1976.py
+++ python/baekjoon_1976.py
@@ -22,7 +22,7 @@ parent = [i for i in range(n+1)]
 
 for i in range(1, n+1):
     temp = list(map(int, sys.stdin.readline().split()))
-    for j in range(1, len(temp)+1):
+    for j in range(1, n+1):
         if temp[j-1] == 1:
             union_parent(parent, i, j)
 
