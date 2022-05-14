d4fc4295349fa73d62c08c0a8724cef07556462b
dawit <dawit0905@gmail.com>
2022-05-15 00:20:19 +0900

    add baekjoon_2096.py

--- python/baekjoon_2096.py
+++ python/baekjoon_2096.py
@@ -1,26 +1,28 @@
 import sys
 
 n = int(sys.stdin.readline())
-min_dp = [[0] * 3 for _ in range(n)]
-max_dp = [[0] * 3 for _ in range(n)]
-data = []
-for i in range(n):
-    data.append(list(map(int, sys.stdin.readline().split())))
+max_dp = [0] * 3
+min_dp = [0] * 3
 
-for i in range(len(data[0])):
-    min_dp[0][i] = data[0][i]
-    max_dp[0][i] = data[0][i]
-for i in range(1, n):
+max_tmp = [0] * 3
+min_tmp = [0] * 3
+for i in range(n):
+    a, b, c = map(int, sys.stdin.readline().split())
+
     for j in range(3):
 
         if j == 0:
-            min_dp[i][j] = min(min_dp[i-1][j], min_dp[i-1][j+1]) + data[i][j]
-            max_dp[i][j] = max(max_dp[i - 1][j], max_dp[i - 1][j + 1]) + data[i][j]
-        elif j == n-1:
-            min_dp[i][j] = min(min_dp[i-1][j-1], min_dp[i-1][j]) + data[i][j]
-            max_dp[i][j] = max(max_dp[i-1][j-1], max_dp[i-1][j]) + data[i][j]
+            max_tmp[j] = a + max(max_dp[j], max_dp[j + 1])
+            min_tmp[j] = a + min(min_dp[j], min_dp[j + 1])
+        elif j == 1:
+            max_tmp[j] = b + max(max_dp[j - 1], max_dp[j], max_dp[j + 1])
+            min_tmp[j] = b + min(min_dp[j - 1], min_dp[j], min_dp[j + 1])
         else:
-            min_dp[i][j] = min(min_dp[i-1][j-1], min_dp[i-1][j], min_dp[i-1][j+1]) + data[i][j]
-            max_dp[i][j] = max(max_dp[i-1][j-1], max_dp[i-1][j], max_dp[i-1][j+1]) + data[i][j]
+            max_tmp[j] = c + max(max_dp[j], max_dp[j - 1])
+            min_tmp[j] = c + min(min_dp[j], min_dp[j - 1])
 
-print(max(max_dp[-1]), min(min_dp[-1]))
+    for j in range(3):
+        max_dp[j] = max_tmp[j]
+        min_dp[j] = min_tmp[j]
+
+print(max(max_dp), min(min_dp))
