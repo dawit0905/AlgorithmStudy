9fdf9e39005e415cc64aad4cc6e03cbb9202833e
dawit <dawit0905@gmail.com>
2022-05-16 13:13:15 +0900

    add baekjoon_11000.py

---
+++ python/baekjoon_11000.py
@@ -0,0 +1,23 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+que = []
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    que.append([a, b])
+
+que.sort()
+
+room = []
+heapq.heappush(room, que[0][1])
+
+for i in range(1, n):
+    if que[i][0] < room[0]:
+        heapq.heappush(room, que[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, que[i][1])
+
+print(len(room))
