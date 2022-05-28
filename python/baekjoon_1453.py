8dfe0afd713242b4726ff50fa84a449b6bc0be59
dawit <dawit0905@gmail.com>
2022-05-29 01:25:46 +0900

    update baekjoon_1453.py

--- python/baekjoon_1453.py
+++ python/baekjoon_1453.py
@@ -1,14 +1,32 @@
+# import sys
+#
+# n = int(sys.stdin.readline())
+# arr = list(map(int, sys.stdin.readline().split()))
+#
+# visited = [0 for _ in range(101)]
+# result = 0
+# for i in arr:
+#     if visited[i] != 0:
+#         result += 1
+#     else:
+#         visited[i] += 1
+#
+# print(result)
+
 import sys
 
 n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
 
 visited = [0 for _ in range(101)]
-result = 0
+
 for i in arr:
-    if visited[i] != 0:
-        result += 1
-    else:
-        visited[i] += 1
+    visited[i] += 1
+
+result = 0
+
+for i in visited:
+    if i >= 2:
+        result += i-1
 
 print(result)
