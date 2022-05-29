bc2cc5218342753989e2d15baa9c96946806e4b4
dawit <dawit0905@gmail.com>
2022-05-29 19:00:25 +0900

    update baekjoon_2605.py

--- python/baekjoon_2605.py
+++ python/baekjoon_2605.py
@@ -1,11 +1,25 @@
+# import sys
+#
+# n = int(sys.stdin.readline())
+# arr = list(map(int, sys.stdin.readline().split()))
+# answer = [0] * n
+# for i in range(1, n+1):
+#     answer.insert(arr[i-1],i)
+# answer.reverse()
+# for i in answer:
+#     if i != 0:
+#         print(i, end=" ")
+
 import sys
 
 n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
-answer = [0] * n
-for i in range(1, n+1):
-    answer.insert(arr[i-1],i)
-answer.reverse()
-for i in answer:
-    if i != 0:
-        print(i, end=" ")
+result = []
+
+cnt = 1
+for i in arr:
+    result.insert(i, cnt)
+    cnt += 1
+
+result.reverse()
+print(*result)
