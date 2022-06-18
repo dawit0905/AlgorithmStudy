0e61a81b4244f06b8bd1bde66723663d94bbfcab
dawit <dawit0905@gmail.com>
2022-06-19 01:54:19 +0900

    update baekjoon_20040.py

--- python/baekjoon_20040.py
+++ python/baekjoon_20040.py
@@ -8,12 +8,9 @@ def find(parent, x):
 
 
 def union(parent, a, b):
-    global answer, idx
     ap = find(parent, a)
     bp = find(parent, b)
 
-    if ap == bp:
-        answer = idx + 1
     if (ap < bp):
         parent[bp] = ap
     else:
@@ -21,12 +18,14 @@ def union(parent, a, b):
 
 
 n, m = map(int, sys.stdin.readline().split())
-edges = []
 answer = 0
 parent = [i for i in range(n)]
 for idx in range(m):
     a, b = map(int, sys.stdin.readline().split())
-    if not answer:
+    if find(parent, a) != find(parent, b):
         union(parent, a, b)
+    else:
+        answer = idx + 1
+        break
 
 print(answer)
