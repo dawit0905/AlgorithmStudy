94db0ffc4ee1e3547b5dcefcb7e5b2b7a2d91604
dawit <dawit0905@gmail.com>
2022-06-26 03:19:45 +0900

    update baekjoon_9655.py

--- python/baekjoon_9655.py
+++ python/baekjoon_9655.py
@@ -2,8 +2,19 @@ import sys
 
 
 n = int(sys.stdin.readline())
+dp = [-1] * 10001
 
-if n % 2 == 0:
-    print('CY')
-else:
+dp[1] = 0 # SK
+dp[2] = 1 # CY
+dp[3] = 0 # SK
+
+for i in range(4, n+1):
+    if dp[i-1] == 0 or dp[i-3] == 0:
+        dp[i] = 1
+    else:
+        dp[i] = 0
+
+if dp[n] == 0:
     print('SK')
+else:
+    print('CY')
