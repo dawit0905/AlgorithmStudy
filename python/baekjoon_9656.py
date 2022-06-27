5bdbc682f249747c8d5bdbb99586091e28febfd2
dawit <dawit0905@gmail.com>
2022-06-27 21:51:15 +0900

    add baekjoon_9656.py

---
+++ python/baekjoon_9656.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 0
+dp[3] = 1
+
+for i in range(4, n+1):
+    if dp[i-1] == 0 or dp[i-3] == 0:
+        dp[i] = 1
+    else:
+        dp[i] = 0
+
+if dp[n] == 0:
+    print('SK')
+else:
+    print('CY')
+
+
