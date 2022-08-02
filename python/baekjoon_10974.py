9b5c12d9632185857e724ffd67dbff96afc7fe11
dawit0905 <dawit0905@gmail.com>
2022-08-02 23:23:22 +0900

    add baekjoon_10974.py

---
+++ python/baekjoon_10974.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def dfs():
+    if len(temp) == n:
+        print(*temp)
+        return
+
+    for i in range(1, n+1):
+        if i not in temp:
+            temp.append(i)
+            dfs()
+            temp.pop()
+
+
+n = int(sys.stdin.readline())
+temp = []
+dfs()
+
