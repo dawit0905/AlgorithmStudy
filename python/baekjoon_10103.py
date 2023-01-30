11a5c2149dd294df028fd2dcdfe29ab5d7f989f1
dawit <dawit0905@gmail.com>
2023-01-30 22:42:56 +0900

    add baekjoon_10103.py

---
+++ python/baekjoon_10103.py
@@ -0,0 +1,16 @@
+import sys
+
+
+n = int(sys.stdin.readline())
+A, B = 100, 100
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+
+    if a > b:
+        B -= a
+    elif a < b:
+        A -= b
+
+print(A)
+print(B)
