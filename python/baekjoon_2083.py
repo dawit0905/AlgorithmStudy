2891695610ac3a0f2810a4faa8d064fa269177bd
dawit <dawit0905@gmail.com>
2023-02-13 00:53:42 +0900

    add baekjoon_2083.py

---
+++ python/baekjoon_2083.py
@@ -0,0 +1,12 @@
+import sys
+
+while True:
+    a, b, c = sys.stdin.readline().split()
+
+    if a == '#':
+        break
+
+    if int(b) > 17 or int(c) >= 80:
+        print(f'{a} Senior')
+    else:
+        print(f'{a} Junior')
