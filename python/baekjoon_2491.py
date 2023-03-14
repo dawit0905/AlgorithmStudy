b40ff1af9f8e0b9a15e291f15752319a4c0debf6
dawit <dawit0905@gmail.com>
2023-03-14 12:39:07 +0900

    update baekjoon_2491.py

--- python/baekjoon_2491.py
+++ python/baekjoon_2491.py
@@ -7,13 +7,12 @@ arr = list(map(int, sys.stdin.readline().split()))
 dp_up = [0] * (n+1)
 dp_down = [0] * (n+1)
 
-# dp_up
+
 for i in range(1, n):
+    # dp_up
     if arr[i-1] <= arr[i]:
         dp_up[i] += dp_up[i-1] + 1
-
-# dp _down
-for i in range(1, n):
+    # dp _down
     if arr[i-1] >= arr[i]:
         dp_down[i] += dp_down[i-1] + 1
 
