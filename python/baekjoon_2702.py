6f23f8a0488881bbd6c0299d8900adeadbc80a7f
dawit <dawit0905@gmail.com>
2023-04-15 01:37:19 +0900

    add baekjoon_2702.py

---
+++ python/baekjoon_2702.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def gcd(a, b):
+
+    if a == 0:
+        return b
+
+    return gcd(b % a, a)
+
+
+def lcm(a, b):
+    pass
+
+
+tc = int(sys.stdin.readline())
+
+for t in range(tc):
+    a, b = map(int, sys.stdin.readline().split())
+    num = gcd(a, b)
+    print(a*b // num, num)
