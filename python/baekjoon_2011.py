ad0ffc0043b6b96ad03affee12aeb19f46a2b9af
dawit0905 <dawit0905@gmail.com>
2023-06-12 17:13:08 +0900

    add baekjoon_2011.py

---
+++ python/baekjoon_2011.py
@@ -0,0 +1,23 @@
+import sys
+
+
+code = [0]
+code += list(sys.stdin.readline().strip())
+
+if code[1] == '0':
+    print(0)
+    exit(0)
+
+length = len(code)
+dp = [0] * length
+dp[0] = dp[1] = 1
+
+for i in range(2, length):
+    first = int(code[i])
+    tenth = int(code[i-1])*10 + int(code[i])
+    if first > 0:
+        dp[i] += dp[i-1]
+    if 10 <= tenth <= 26:
+        dp[i] += dp[i - 2]
+
+print(dp[length-1]%1000000)
