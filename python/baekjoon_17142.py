d345dc465f415d0ea56dc34849e575b6c2667a82
dawit0905 <dawit0905@gmail.com>
2023-07-18 18:34:01 +0900

    update baekjoon_17142.py

--- python/baekjoon_17142.py
+++ python/baekjoon_17142.py
@@ -3,7 +3,7 @@ from collections import deque
 from itertools import combinations
 
 
-def bfs(combi):
+def bfs(combi, leftArea):
     global wall_cnt
     que = deque()
     # -1:초기값, 0:바이러스 시작 위치, 1,2,3,4,5 ... 은 바이러스의 증가값들
@@ -16,6 +16,9 @@ def bfs(combi):
 
     result = 0
     while que:
+        if not leftArea:
+            break
+
         x, y, cost = que.popleft()
 
         for i in range(4):
@@ -23,16 +26,21 @@ def bfs(combi):
             ny = y + dy[i]
 
             if 0 <= nx < n and 0 <= ny < n:
+                # 방문하지 않았고, 빈칸일때
                 if visited[nx][ny] == -1 and graph[nx][ny] == 0:
                     que.append((nx, ny, cost+1))
                     visited[nx][ny] = cost+1
                     result = max(result, visited[nx][ny])
+                # 방문하지 않았고,
                 elif visited[nx][ny] == -1 and graph[nx][ny] == 2:
                     que.append((nx, ny, cost+1))
                     visited[nx][ny] = cost+1
 
+    if not leftArea:
+        return result
+
     if list(sum(visited, [])).count(-1) != wall_cnt:
-        return None
+        return sys.maxsize
     return result
 
 
@@ -45,21 +53,29 @@ for i in range(n):
     graph.append(list(map(int, sys.stdin.readline().split())))
 
 bias_index = []
+bias_cnt = 0
 wall_cnt = 0
+left = 0
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 1:
+
+        if graph[i][j] == 0:
+            left += 1
+
+        elif graph[i][j] == 1:
             wall_cnt += 1
 
         elif graph[i][j] == 2:
             bias_index.append((i, j))
+            bias_cnt += 1
+
+
 
 combis = list(combinations(bias_index, m))
 
 answer = sys.maxsize
 for combi in combis:
-    return_num = bfs(combi)
-    if return_num != None:
-        answer = min(answer, bfs(combi))
+    answer = min(answer, bfs(combi, left))
 
 print(-1) if answer == sys.maxsize else print(answer)
+
