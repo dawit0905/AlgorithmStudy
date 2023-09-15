106b1300e042b12b5d074739b064f440853ad465
dawit0905 <dawit0905@gmail.com>
2023-09-15 10:35:22 +0900

    add baekjoon_1080.py

---
+++ python/baekjoon_1080.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def reverse(x, y):
+    for i in range(x, x+3):
+        for j in range(y, y+3):
+            A[i][j] = 1 - A[i][j]
+
+def check():
+    for i in range(n):
+        for j in range(m):
+            if A[i][j] != B[i][j]:
+                return False
+
+    return True
+
+
+n, m = map(int, sys.stdin.readline().split())
+A = [list(map(int, list(sys.stdin.readline().strip()))) for _ in range(n)]
+B = [list(map(int, list(sys.stdin.readline().strip()))) for _ in range(n)]
+
+count = 0
+for i in range(n-2):
+    for j in range(m-2):
+        if A[i][j] != B[i][j]:
+            reverse(i, j)
+            count += 1
+
+if check():
+    print(count)
+else:
+    print(-1)
+
+
