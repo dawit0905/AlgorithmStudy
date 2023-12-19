a547cdec62ffaabab644390ea2148206a44ac081
dawit0905 <dawit0905@gmail.com>
2023-12-19 15:32:52 +0900

    add baekjoon_13241.py

---
+++ python/baekjoon_13241.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+
+
+a, b = map(int, sys.stdin.readline().split())
+
+GCD = gcd(b, a)
+LCM = a*b // GCD
+
+print(LCM)
